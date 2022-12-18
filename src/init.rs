6f91bf3a8a8821c50ef3e45f9a98708156828d8f
Vladimir <azuresplash@gmail.com>
2022-12-18 22:09:36 +0100

    Added random resources and construction sites

--- src/init.rs
+++ src/init.rs
@@ -4,29 +4,30 @@ use bevy::{
     hierarchy::BuildChildren,
     math::{Vec2, Vec3},
     prelude::{
-        App, Bundle, Commands, Entity, Plugin, Res, SystemSet,
-        Transform, Camera2dBundle, Resource,
+        App, Bundle, Camera2dBundle, Commands, Entity, Plugin, Res, Resource, SystemSet, Transform,
     },
     sprite::{Sprite, SpriteBundle},
 };
 use rand::Rng;
 
 use crate::{
-    worker_job_tooltip::{create_tooltip_bundle, WorkerJobTooltip},
     building::{
-        convert_construction_site_to_building, spawn_construction_site, BuildingTextureSet,
+        convert_construction_site_to_building, get_construction_site_texture,
+        spawn_construction_site, BuildingTextureSet, BuildingBlueprint,
     },
     loading::{FontAssets, TextureAssets},
     monkey_planner::MonkeyPlanner,
     movement::{hack_3d_position_to_2d, Position, Walker},
+    planned_work::BUILDING_JOB_NAME,
     planting_crops::{plan_farm_field, PLANTING_JOB_NAME},
     resource_gathering::plan_resource_gathering,
-    resources::ResourceCarrier,
+    resources::{spawn_resource, ResourceCarrier, ResourceChunk, ResourceKind},
     skills::{SkillType, Skilled},
     stockpile::spawn_stockpile,
     tree::spawn_tree,
     tree_cutting_job::{plan_tree_cutting, TREE_CUTTING_JOB_NAME},
-    GameState, planned_work::BUILDING_JOB_NAME,
+    worker_job_tooltip::{create_tooltip_bundle, WorkerJobTooltip},
+    GameState,
 };
 
 pub struct InitPlugin;
@@ -54,36 +55,45 @@ fn init(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    for _ in 0..1 {
-        let pos = get_random_pos(Vec2::ZERO, world_params.size / 2.0);
-        let worker_id = spawn_worker(&mut commands, &textures, &fonts, pos);
+    // for _ in 0..1 {
+    //     let pos = get_random_pos(Vec2::ZERO, world_params.size / 2.0);
+    //     let worker_id = spawn_worker(&mut commands, &textures, &fonts, pos);
 
-        let work_id = MonkeyPlanner::plan_house(
-            &mut commands,
-            &textures,
-            get_random_pos(Vec2::ZERO, world_params.size / 4.0),
-        );
+    //     let work_id = MonkeyPlanner::plan_house(
+    //         &mut commands,
+    //         &textures,
+    //         get_random_pos(Vec2::ZERO, world_params.size / 4.0),
+    //     );
 
-        MonkeyPlanner::temp_recruit_workers(&mut commands, work_id, vec![worker_id], BUILDING_JOB_NAME)
-    }
+    //     MonkeyPlanner::temp_recruit_workers(
+    //         &mut commands,
+    //         work_id,
+    //         vec![worker_id],
+    //         BUILDING_JOB_NAME,
+    //     )
+    // }
 
     {
         let pos = get_random_pos(Vec2::ZERO, world_params.size / 2.0);
         spawn_stockpile(&mut commands, pos, Vec2::new(100.0, 100.0));
-        
     }
 
-    for _ in 0..1 {
-        let pos = get_random_pos(Vec2::ZERO, world_params.size / 2.0);
-        let worker_id = spawn_worker(&mut commands, &textures, &fonts, pos);
+    // for _ in 0..1 {
+    //     let pos = get_random_pos(Vec2::ZERO, world_params.size / 2.0);
+    //     let worker_id = spawn_worker(&mut commands, &textures, &fonts, pos);
 
-        let work_id = plan_farm_field(
-            &mut commands,
-            get_random_pos(Vec2::ZERO, world_params.size / 4.0),
-        );
+    //     let work_id = plan_farm_field(
+    //         &mut commands,
+    //         get_random_pos(Vec2::ZERO, world_params.size / 4.0),
+    //     );
 
-        MonkeyPlanner::temp_recruit_workers(&mut commands, work_id, vec![worker_id], PLANTING_JOB_NAME)
-    }
+    //     MonkeyPlanner::temp_recruit_workers(
+    //         &mut commands,
+    //         work_id,
+    //         vec![worker_id],
+    //         PLANTING_JOB_NAME,
+    //     )
+    // }
 
     for _ in 0..2 {
         let worker_pos = get_random_pos(Vec2::ZERO, world_params.size / 3.0);
@@ -93,13 +103,32 @@ fn init(
         let tree_id = spawn_tree(&mut commands, &textures, tree_pos);
         let work_id = plan_tree_cutting(&mut commands, tree_id);
 
-        MonkeyPlanner::temp_recruit_workers(&mut commands, work_id, vec![worker_id], TREE_CUTTING_JOB_NAME)
+        // MonkeyPlanner::temp_recruit_workers(
+        //     &mut commands,
+        //     work_id,
+        //     vec![worker_id],
+        //     TREE_CUTTING_JOB_NAME,
+        // )
     }
 
-    for _ in 0..1 {
-        let worker_pos = get_random_pos(Vec2::ZERO, world_params.size / 3.0);
-        let worker_id = spawn_worker(&mut commands, &textures, &fonts, worker_pos);
-        plan_resource_gathering(&mut commands, worker_id);
+    // for _ in 0..1 {
+    //     let worker_pos = get_random_pos(Vec2::ZERO, world_params.size / 3.0);
+    //     let worker_id = spawn_worker(&mut commands, &textures, &fonts, worker_pos);
+    //     plan_resource_gathering(&mut commands, worker_id);
+    // }
+
+    for _ in 0..10 {
+        let position = get_random_pos(Vec2::ZERO, world_params.size / 3.0);
+        spawn_resource(
+            &mut commands,
+            &textures,
+            ResourceChunk {
+                kind: crate::resources::ResourceKind::Wood,
+                quantity: 5,
+            },
+            position,
+            false,
+        );
     }
 
     let house_textures = BuildingTextureSet {
@@ -109,11 +138,30 @@ fn init(
     };
 
     for _ in 0..5 {
-        let pos = get_random_pos(Vec2::ZERO, world_params.size / 2.0);
+        let pos = get_random_pos(Vec2::ZERO, world_params.size / 4.0);
 
         let construction_site_id = commands.spawn_empty().id();
         spawn_construction_site(&mut commands, construction_site_id, pos, &house_textures);
-        convert_construction_site_to_building(construction_site_id, &mut commands, &house_textures);
+        let building_blueprint = BuildingBlueprint {
+            name: "House",
+            max_hp: 2000.0,
+            units_of_work: 100.0,
+            texture_set: BuildingTextureSet {
+                in_progress: vec![
+                    textures.house_in_progress.clone(),
+                    textures.house_in_progress.clone(),
+                ],
+                completed: textures.house.clone(),
+                scale: 0.03,
+            },
+            max_workers: 2,
+            required_resources: vec![(ResourceKind::Wood, 4)],
+        };
+        if let Some(new_texture) = get_construction_site_texture(0.0, 0.1, &building_blueprint) {
+            commands.entity(construction_site_id).insert(new_texture);
+        }
+
+        // convert_construction_site_to_building(construction_site_id, &mut commands, &house_textures);
     }
 
     for _ in 0..40 {
@@ -171,11 +219,7 @@ fn spawn_worker(
         .insert(Position(position))
         .insert(ResourceCarrier { max_volume: 120 })
         .with_children(|parent| {
-            id = Some(
-                parent
-                    .spawn(create_tooltip_bundle(13.0, &fonts))
-                    .id(),
-            );
+            id = Some(parent.spawn(create_tooltip_bundle(13.0, &fonts)).id());
         })
         .insert(WorkerJobTooltip {
             title: "".to_string(),
